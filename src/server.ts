cac7c93c0e1d8428e62e2ffe18dafedb1f56e89a
arthurwillame5 <135058422+arthurwillame5@users.noreply.github.com>
2024-05-22 19:16:23 -0300

    chore: Update .gitignore and add knexfile.ts, package.json, and database.ts

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,17 @@
 import fastify from "fastify";
 import { knex } from "./database";
+import crypto from "node:crypto";
 
 const app = fastify();
 
-app.get("/hello", async () => {
-    const test = await knex('sqlite_schema').select('*')
-
-    return test
-});
+app.get("/", async () => {
+  const transaction = await knex("transactions").insert({
+    id: crypto.randomUUID(),
+    title: "Transação de teste",
+    amount: 1000
+  }).returning("*")
+  return transaction;
+})
 
 app
   .listen({
